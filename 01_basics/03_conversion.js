c0b0a3f13e142c252885845cc6a085c493bced5c
Pratham <prathamdangaich@gmail.com>
2025-05-14 19:48:29 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,16 @@
+let score="33abc";
+//let score=null;
+//let score=undefined;
+console.log(typeof score);
+let value=Number(score);
+console.log(typeof value);
+console.log(value);
+
+
+let isLogged=0;
+// let isLogged="";
+//let isLogged="Pratham"
+console.log(typeof isLogged);
+let boolLogged=Boolean(isLogged);
+console.log(typeof boolLogged);
+console.log(boolLogged);
