672b526236f7f5522fa300ae5059b8e3fc34d590
Pratham <prathamdangaich@gmail.com>
2025-05-15 10:31:42 +0530

    conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -14,3 +14,11 @@ console.log(typeof isLogged);
 let boolLogged=Boolean(isLogged);
 console.log(typeof boolLogged);
 console.log(boolLogged);
+
+console.log(1+2+3+"3"+2+2);
+console.log(+true);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null===0);
+console.log(null==undefined);
